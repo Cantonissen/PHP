f53b13a7bd899653383b5cea6268787773d17024
Cantonissen <54833786+Cantonissen@users.noreply.github.com>
2020-03-17 10:25:55 +0100

    initial commit PHP

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-echo "hello world!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cursus PHP</title>
+</head>
+<body>
+<a href="h01/opdracht1.php">Hoofdstuk 1</a>
+
+</body>
+</html>
